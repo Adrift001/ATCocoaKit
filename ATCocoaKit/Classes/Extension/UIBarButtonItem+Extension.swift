59c5e7bdd80a54cd8c351c4eff7116c7059deb03
Optional <jingxuetao@qq.com>
2020-03-26 11:15:16 +0800

    UIBarButtonItem_convenience

---
+++ ATCocoaKit/Classes/Extension/UIBarButtonItem+Extension.swift
@@ -0,0 +1,51 @@
+//
+//  UIBarButtonItem+Extension.swift
+//  ATCocoaExtension
+//
+//  Created by swifter on 2020/3/26.
+//  Copyright © 2020 swifter. All rights reserved.
+//
+
+import UIKit
+
+public extension UIBarButtonItem {
+        
+    private struct AssociatedKeys {
+        static var imageKey = "imageKey"
+        static var titleKey = "titleKey"
+    }
+    
+    /// 图片初始化边界构造器
+    /// - Parameters:
+    ///   - image: 图片
+    ///   - style: 风格
+    ///   - handler: 点击回调
+    convenience init(image: UIImage?, style: UIBarButtonItem.Style, handler: @escaping NormalClosure) {
+        self.init(image: image, style: style, target: nil, action: #selector(imageActionClosure))
+        target = self
+        objc_setAssociatedObject(self, &AssociatedKeys.imageKey, handler, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+    
+    @objc func imageActionClosure() {
+        if let closure = objc_getAssociatedObject(self, &AssociatedKeys.imageKey) as? NormalClosure {
+            closure()
+        }
+    }
+    
+    /// 文字初始化便捷构造器
+    /// - Parameters:
+    ///   - title: 文字
+    ///   - style: 风格
+    ///   - handler: 点击回调
+    convenience init(title: String?, style: UIBarButtonItem.Style, handler: @escaping NormalClosure) {
+        self.init(title: title, style: style, target: nil, action: #selector(titleActionClosure))
+        target = self
+        objc_setAssociatedObject(self, &AssociatedKeys.titleKey, handler, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+    
+    @objc func titleActionClosure() {
+        if let closure = objc_getAssociatedObject(self, &AssociatedKeys.titleKey) as? NormalClosure {
+            closure()
+        }
+    }
+}
